e62902de3a2b0c02e579dbe87fb32e6880061c4b
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2018-10-03 10:35:46 -0500

    with nyt disclaimer

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,8 +6,9 @@ const Nav = () => (
       <strong className="navbar-brand mx-auto">
         <i className="fa fa-newspaper-o">
           <span>
-            &nbsp; <a className="portLink" href="https://usa85.github.io">New York Times Collection
-            </a>
+            &nbsp; <a className="portLink" href="https://www.nytimes.com/">The New York Times Connection
+            </a><br>
+            This site is about retrieving, for educational purposes only, some of the articles published by the New York Times</br>
         </span>
         </i>
       </strong>
