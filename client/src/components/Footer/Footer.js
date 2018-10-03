084a62b6a5c8ef022f98f6e22dbff7307418f7b1
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2018-10-03 10:29:34 -0500

    with pictures

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
     <div className="bg-success row">
         <div className="col-sm-12 ">
             <hr></hr>
-                <h5 className="text-center textun">
+                <h5 className="text-center footertext">
                     <small><a className="portLink " href="https://usa85.github.io">Github Portfolio</a> and/or <a className="portLink" href="http://www.capija.com">Go Daddy's Hosting</a> and/or <a className="portLink" href="https://www.linkedin.com/in/pierrejcallies/?locale=en_US">Linkendin Profile</a>
                     </small>
                 </h5>
